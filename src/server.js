94b1fd01a020467e0c507c89dbcd88b9bfa24eaa
Hiroa <hirroa@gmail.com>
2022-06-27 20:07:53 +0200

    [DEPLOY] Add automatic deployment

--- src/server.js
+++ src/server.js
@@ -2,18 +2,24 @@ const express = require("express");
 const reports = require('./routes/reports');
 const swaggerUi = require('swagger-ui-express')
 const openApiDocumentation = require('../openApiDocumentation');
+const pjson = require('../package.json');
 
 const app = express();
 const port = process.env.PORT || 3000;
+const sha = process.env.SHA || "Dev";
 
 app.use(express.json());
 
 app.use('/reports', reports)
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(openApiDocumentation));
 
+app.get('/', (req, res) => {
+    res.json({versions: pjson.version, sha: sha});
+});
+
 app.get("*", (req, res) => {
     res.sendStatus(404);
 });
 
-app.listen(port, () => console.log(`ZodiacBuddyDB app listening on port ${port}!`))
+app.listen(port, () => console.log(`ZodiacBuddyDB app V${pjson.version}.${sha} listening on port ${port}!`))
 
