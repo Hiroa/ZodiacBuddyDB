6baa41c620c3f2651cf40c604c9aa00466fb4958
Hiroa <hirroa@gmail.com>
2023-04-01 14:31:35 +0200

    [DEPLOY] Add jwt claim version verification

--- src/security/security.js
+++ src/security/security.js
@@ -8,6 +8,8 @@ const options = {
     audience: ['ZodiacBuddy']
 }
 
+const minVersion = 2
+
 exports.checkJWT = async (req, res, next) => {
     let token = req.headers['x-access-token'] || req.headers['authorization'];
     if (!!token && token.startsWith('Bearer ')) {
@@ -22,9 +24,14 @@ exports.checkJWT = async (req, res, next) => {
         if (err) {
             console.info(`Invalid token: ${err.message}`)
             return res.status(401).json('Access token is missing or invalid');
-        } else if (!decoded.sub) {
+        }
+        if (!decoded.sub) {
             console.info(`Missing sub in token: ${JSON.stringify(decoded)}`)
             return res.status(401).json('Access token is missing or invalid');
+        }
+        if (!decoded.version || !Number(decoded.version) || minVersion > decoded.version) {
+            console.info(`Invalid jwt claim version: ${JSON.stringify(decoded)}`)
+            return res.status(401).json('Invalid version, please update your plugin');
         }
         req.cid = decoded.sub;
         req.aud = decoded.aud;
